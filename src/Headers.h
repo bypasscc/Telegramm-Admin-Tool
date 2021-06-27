f5b5f04e89048d396a784248480756aff55132e0
Retr0Wawe <74567717+Retr0Wawe@users.noreply.github.com>
2021-06-27 14:06:54 +0300

    Add files via upload

---
+++ src/Headers.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include <wininet.h>
+#include <TlHelp32.h>
+#include "nlohmann/json.hpp"
+
+#pragma comment(lib, "WinInet.lib")
+#pragma warning(disable : 4996)

bf2d3e153a5cc3b3c6ecb8fda66e6e4fd7e23afa
Retr0Wawe <74567717+Retr0Wawe@users.noreply.github.com>
2021-06-27 14:02:41 +0300

    Delete src directory

--- src/Headers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <Windows.h>
-#include <wininet.h>
-#include <TlHelp32.h>
-#include "nlohmann/json.hpp"
-
-#pragma comment(lib, "WinInet.lib")
-#pragma warning(disable : 4996)
